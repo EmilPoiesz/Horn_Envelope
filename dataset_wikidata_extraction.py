b8507962c3da9334e4ece0a3361997c8ed1ea066
Emil Poiesz <poiesz92@gmail.com>
2024-12-06 08:44:38 +0100

    Scale issue with queries

--- dataset_wikidata_extraction.py
+++ dataset_wikidata_extraction.py
@@ -1,6 +1,5 @@
 import requests 
 import json
-import sys
 import pandas as pd
 from helper_functions import *
 
@@ -47,12 +46,11 @@ def buildquery(occupation, limit=1_000_000, offset=0):
     """
     return basequery.format(occupationID = occupation, limit=limit, offset=offset)
 
-import requests
 
-url = "https://query.wikidata.org/sparql"
-headers = {"Accept": "application/json"}
+def fetch_data(occupation_id, occupation_name, limit=10_000):
+    # For very large queries it is good practice to break it up 
+    # into smaller queries to prevent overloading the service.
 
-def fetch_data(occupation_id, occupation_name, limit=1_000):
     offset = 0
     all_results = []
     
@@ -62,13 +60,11 @@ def fetch_data(occupation_id, occupation_name, limit=1_000):
         response = requests.get(url, params={'query': query, 'format': 'json'}, headers=headers)
         
         try:
-            # Decode JSON response
             data = response.json()
-            bindings = data.get('results', {}).get('bindings', [])
+            bindings = data['results']['bindings']
             
             # Stop the loop if no more results are returned
-            if not bindings:
-                break
+            if not bindings: break
             
             all_results.extend(bindings)
             offset += limit  # Increase the offset for the next batch
@@ -76,47 +72,36 @@ def fetch_data(occupation_id, occupation_name, limit=1_000):
         except requests.exceptions.JSONDecodeError:
             print(f"Failed to decode JSON for {occupation_name}. Response status code:")
             print(response.status_code)
-            print(response.text)  # Print raw response for debugging
+            print(response.text) 
             break
 
-    return data, all_results
+    return all_results
 
 
 if __name__ == "__main__":
-    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
 
-    # running automated queries we need to add 'bot' to the name of the agent. Also provide a way to be contacted (like an email).
+    # When running automated queries we need to add 'bot' to the name of the agent. 
+    # Also provide a way to be contacted (like an email).
+    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
     headers  = {'User-Agent' : 'HornEnvelopeLearnerOccupationRetrivalQueryBot (emilpo@uio.no)'} 
     occ_lst_filename = "occupations.csv"
 
-    # import occupations from csv.
+    # Read list of occupations from csv.
     occupation_list = pd.read_csv(occ_lst_filename, header=None).to_numpy()
     occupation_list_clean = []
 
-    #query all occupations
+    # Query all occupations
     for occupation in occupation_list:
         occupation_id   = occupation[1]
         occupation_name = occupation[0]
             
-        # Send a request to Wikidata
-        #response = requests.get(url, params={'query': buildquery(occupation_id), 'format': 'json'}, headers=headers)
-        #try:
-        #    data = response.json()
-        #except requests.exceptions.JSONDecodeError:
-        #    print(f"Failed to decode JSON for {occupation_name}. Response status code:")
-        #    print(response.status_code)
-        #    continue
-        
-        #results = data['results']['bindings']
-        data, results = fetch_data(occupation_id, occupation_name)
+        results = fetch_data(occupation_id, occupation_name)
         print("Queried for " + occupation_name + " and found " + str(len(results)) + " results in wikidata.")
         
-        #if there are results, continue execution
         if len(results) > 0:
             occupation_data = []
             
             for item in results:
-
                 occupation_data.append({
                     'name': return_if_exists(item, 'individual'),
                     'gender': return_if_exists(item, 'gender'),
@@ -125,20 +110,18 @@ if __name__ == "__main__":
                     'nationalityID': get_nid(return_if_exists(item, 'nationalityID'))
                 })
 
-            #if there are results after the cleanup, continue execution
-            if len(occupation_data) > 0:
-                #save cleaned dataframe 
-                file = 'data/data_dataframes/' + occupation_name + '.csv'
-                df = pd.DataFrame(occupation_data)
-                #df = df.drop_duplicates(subset='name', keep='first')
-                print("\tAfter cleanup: " + str(len(df)) + " datapoints left")
-                df.to_csv(file, index=False)
-                #save uncleaned raw data seperately
-                occ_file = 'data/dataset_raw/' + occupation_name + '.json'
-                with open(occ_file, 'w') as outfile:
-                    json.dump(data, outfile)
-                #keep track of occupations that are actually saved as datapoints
-                occupation_list_clean.append(occupation)
+            # Save the dataframe as CSV
+            file = 'data/data_dataframes/' + occupation_name + '.csv'
+            df = pd.DataFrame(occupation_data)
+            df.to_csv(file, index=False)
+
+            # Save raw JSON data
+            occ_file = 'data/dataset_raw/' + occupation_name + '.json'
+            with open(occ_file, 'w') as outfile:
+                json.dump(results, outfile)
+
+            # Keep track of stored occupations
+            occupation_list_clean.append(occupation)
 
     df_occupations = pd.DataFrame(occupation_list_clean)
     df_occupations.to_csv('data/occupations_updated.csv', index=None, header=None)
