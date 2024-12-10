3de7af0bb1b6d1290c64943fb2b74977b75723ff
Emil Poiesz <poiesz92@gmail.com>
2024-12-10 10:09:50 +0100

    Refactored generation of training sentences.

--- dataset_sentence_creation.py
+++ dataset_sentence_creation.py
@@ -1,12 +1,12 @@
+######################################################################################
+#                                                                                    #
+#    Generates sentence/label pairs for every datapoint extrtacted from wikidata.    #
+#                                                                                    #
+######################################################################################
+
 import pandas as pd
 import os
 import sys
-from helper_functions import *
-
-"""
-    Goes through all the data files and creates sentence/label pairs
-    This is, what can be fed into the language model
-"""
 
 def load_occupations():
     # If dataset_wikidata_extraction.py has not been run before this file then we need to throw an error.
@@ -15,32 +15,28 @@ def load_occupations():
     
     print('No occupations have been extracted yet, you first need to run the file dataset_wikidata_extraction.py.')
     sys.exit(0)
-    
 
+def get_pronoun(gender):
+    is_female = gender == 'female' or gender == 'transgender female' or gender == 'cisgender female' or gender == 'trans woman'
+    is_male   = gender == 'male'   or gender == 'transgender male'   or gender == 'cisgender male'   or gender == 'trans man'
+    return 'female' if is_female else 'male' if is_male else 'other'
+    
 if __name__ == "__main__":
 
-    sentence = "<mask> was born in {birthyear} in {nationality} and is a {occupation}."
-    
-    occupations = load_occupations()
+    query_sentence = "<mask> was born in {birthyear} in {nationality} and is a {occupation}."
+    occupations   = load_occupations()
 
-    
-    # Load list of known countries.
-    if os.path.exists('data/nIDs_valid.csv'):
-        nIDs_valid = pd.read_csv('data/nIDs_valid.csv').set_index('nID')['is_valid'].to_dict()
-    else:
-        nIDs_valid = {}
-
-    country_list = pd.read_csv('data/countries_with_nids_grammar.csv', header=None).to_numpy()
-    country_dict= {row[0]: row[1] for row in country_list}
-
-    for occupation in occupations:
-        id = occupation[1]
+    for idx, occupation in occupations.iterrows():
         name = occupation[0]
-        occ_file = 'data/dataframes_cleaned/' + name + '.csv'
-        df = pd.read_csv(occ_file)
-        sentence_list = []
+        
+        df = pd.read_csv(f'data/csv_clean/{name}.csv')
+        sentences = []
         for index,row in df.iterrows():
-            s = sentence.format(birthyear=row['birth'], nationality=country_dict[row['nid']], occupation=name.replace('_', ' '))
-            sentence_list.append([s, get_pronoun(row['gender'])] )
-        sentence_df = pd.DataFrame(sentence_list)
+            sentence = query_sentence.format(birthyear=row['birth'], nationality=row['nationality'], occupation=name.replace('_', ' '))
+            pronoun = get_pronoun(row['gender'])
+            
+            # Store sentence with correct pronoun as a traning example
+            sentences.append([sentence, pronoun] )
+
+        sentence_df = pd.DataFrame(sentences)
         sentence_df.to_csv('data/dataset.csv', mode='a',index=False, header=False)
