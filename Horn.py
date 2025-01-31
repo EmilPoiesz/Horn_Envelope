f9c8ed1b365c2600b4a81c0bbd0d7f571f1cbd34
Emil Poiesz <emilpo@uio.no>
2025-01-31 11:15:26 +0100

    Remove unused methods

--- Horn.py
+++ Horn.py
@@ -1,6 +1,5 @@
 from sympy import *
 import functools
-import random
 import timeit
 import Binarizer
 
@@ -16,32 +15,12 @@ def define_variables(number):
     """
     return list(symbols("".join(['v'+str(i)+',' for i in range(number)])))
 
-#TODO: This is not used?
-def generate_target(V, n_clauses,n_body_literals=-1):
-    T = set()
-    for i in range(n_clauses):
-        if n_body_literals < 0:
-            V_sample = random.sample(V, random.randint(1, len(V)-1))
-        else:
-            V_sample = random.sample(V, n_body_literals)
-        clause = functools.reduce(lambda x,y: x & y, V_sample)
-        V_implies = [item for item in V if item not in V_sample]
-        x = random.randint(0, len(V)+1)
-        target = ((clause) >> false if x == 0
-                  else true >> (clause) if x == len(V)+1 and len(V_sample) == 1
-                  else (clause) >> random.choice(V_implies))
-        T.add(target)
-    return T
-
 def evaluate(clause, x, V):
     
-    #Check if clause is tautology or contradiction
+    #tautology or contradiction
     if clause == True or clause == False: return clause 
 
     assignment = {V[i]: x[i] for i in range(len(V))}
-    #for i in range(len(V)):
-    #    a[V[i]] = (True if x[i] == 1
-    #                    else False)
     return clause.subs(assignment)
 
 def from_set_to_theory(set):
@@ -54,119 +33,8 @@ def from_set_to_theory(set):
     Returns:
         Expr: The result of performing a logical AND operation on all elements in the set.
     """
-    #TODO: Can we use functools.reduce(lambda: ... ) like in get_hypothesis?
-    #combined_clause = True
-    #for clause in set:
-    #    combined_clause = combined_clause & clause
-    #return combined_clause
+
     return functools.reduce(lambda x,y: x & y, set)
-
-#TODO: This is not used?
-def entails(T,clause,V):
-    '''
-    Checks if T entails a horn clause c.
-    Returns an assignment that falsifies c and satisfies T
-     if the check is false.
-    '''
-    T1 = from_set_to_theory(T)
-    assignment =satisfiable(T1 & ~clause)
-    res =[0 for i in range(len(V))]
-    if assignment != False :
-        for e in assignment:
-            idx = V.index(e)
-            res[idx] =  1 if assignment[e] == True else 0
-        return res
-    return True
-
-#TODO: This is not used?
-def EQ(H, V, target):
-    for c in H:
-        answer = entails(target,c,V)
-        if  answer != True:
-            #here a positive counterexample is returned
-            return answer
-    for c in target:
-        answer = entails(H,c,V)
-        if  answer != True:
-            #here a negative counterexample is returned
-            return answer
-    return True
-
-#TODO: This is not used?
-def MQ(assignment, V, target):
-    t = from_set_to_theory(target)
-    return evaluate(t,assignment,V)
-
-#TODO: This is not used?
-def get_hypothesis(negative_counterexamples, V, excluded_negative_counterexamples, background):
-    H = set()
-    for assignment in [example for example in negative_counterexamples if example not in excluded_negative_counterexamples]:
-        # This seems like a weird mix of horn(e) and quasi(e)
-        # Each assignment is in negative counterexamples meaning that it seems like we are in line 14, creating H.
-        L = [V[index] for index,value in enumerate(assignment) if assignment[index] ==1 ] + [true]
-        R = [V[index] for index,value in enumerate(assignment) if assignment[index] ==0 ] + [false]
-        for r in R:
-            # But here we take each true value and imply each false value in turn. 
-            # Same as conjunction of true -> disjunction of false?
-            # But this is the method for the quasi section which should be used on non-horn examples
-            # Are bad_nc/excluded_negative_counterexamples the same as non-horn?
-            # This is a bit confusing.
-            clause = functools.reduce(lambda x,y: x & y, L)
-            clause = (clause) >> r
-            H.add(clause)
-    H = H.union(background)
-    return H
-
-#TODO: this is not used?
-def get_body(clause):
-    if type(clause.args[0]) == Symbol:
-        return (clause.args[0],)
-    else: return clause.args[0].args
-
-#TODO: This is not used?
-def positive_check_and_prune(H,S,Pos,V,bad_nc):
-    for pos in Pos:
-        for clause in H.copy():
-            if (evaluate(clause, pos, V) == False):
-                H.remove(clause)
-                # identify_problematic_nc(H,S,bad_nc)
-
-    identify_problematic_nc(H,S,bad_nc,V)
-
-    # for clause in [c for c in H.copy() if type(c) == Implies]:
-    #     for c2 in [c for c in H.copy() if  type(c) == Not]:
-    #         if set(get_body(clause)).issubset(set(get_body(clause))):
-    #             H.discard(clause)
-
-    return H
-
-#TODO: This is not used?
-def checkduplicates(x,S,enabled):
-    if x in S and enabled:
-        # print('ah! I am trying to add {} to S but the negative counterexample {} is already present in S.'.format(x,x))
-        raise Exception('I am trying to add {} to S but the negative counterexample {} is already present in S.\nIf you are learning from examples classified by a neural network, it means that it is not encoding a horn theory.'.format(x,x))
-
-#TODO: This is not used?
-def identify_problematic_nc(H,S,bad_nc,V):
-    # I am not sure what this does as it is not part of the algorithm so I have commented out the one use of this method.
-
-    # Is nc -> Non-norn Clause?
-
-    # Current hypothesis
-    h = from_set_to_theory(H)
-    # List of negative? counterexamples that cannot produce a rule (according to positive counterexamples)?
-    for a in [a for a in S if a not in bad_nc]: # Why bad? What does bad mean here?
-        # If they are true, then are they negative counterexamples?
-        # This feels like something that is correlated to NON-HORN section of the algorithm?
-        if (evaluate(h, a, V) == True):
-            bad_nc.append(a)
-
-#TODO: This is not used?
-def is_going_to_be_duplicate(negative_counterexamples,example_intersection,bad_nc):
-    if example_intersection in negative_counterexamples:
-        bad_nc.append(example_intersection)
-        return True
-    else: return False
 
 def is_subset(subset, superset):
     return all(x <= y for x, y in zip(subset, superset))
